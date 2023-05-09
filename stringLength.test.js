9b60828640a3351308523ab2985d4bb3d0d0e6dd
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-05-09 12:31:09 -0600

    Create unit test stringLegth.text.js for stringLength.js

---
+++ stringLength.test.js
@@ -0,0 +1,6 @@
+test('string length return the correct length of string', () => { 
+  expect(stringLength('hello')).toBe(5);
+  expect(stringLength('hello world')).toBe(11);
+  expect(stringLength('')).toBe(0);
+  expect(stringLength('123456789')).toBe(9);
+});
