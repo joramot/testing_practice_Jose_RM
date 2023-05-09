062bdae7d141db5c833bdd03833f335fd9e84aa0
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-05-09 13:29:52 -0600

    Expand the function to make it check if the string is at least 1 character long and not longer than 10 characters. Throw errors if those conditions are not met.

--- stringLength.test.js
+++ stringLength.test.js
@@ -6,3 +6,19 @@ test('string length return the correct length of string', () => {
   expect(stringLength('')).toBe(0);
   expect(stringLength('123456789')).toBe(9);
 });
+
+test('stringLength throws an error if string is too short', () => {
+  expect(() => {
+    stringLength('');
+  }).toThrow('String must be between 1 and 10 characters long');
+});
+
+test('stringLength throws an error if string is too long', () => {
+  expect(() => {
+    stringLength('12345678901');
+  }).toThrow('String must be between 1 and 10 characters long');
+});
+
+test('stringLength returns correct length for valid string', () => {
+  expect(stringLength('hello')).toBe(5);
+});
